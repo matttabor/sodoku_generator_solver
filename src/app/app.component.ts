3c75b12540a5fb80ce54a4314717097ca85512f2
matthew tabor <matthew.tabor@itrellis.com>
2020-03-25 20:46:57 -0700

    cleanup ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,6 @@ export class AppComponent {
 
   constructor() {
     this.initializeEmptyBoard();
-    // this.board = [
-    //   [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    //   [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    //   [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    //   [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    //   [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    //   [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    //   [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    //   [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    //   [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    // ];
   }
 
   initializeEmptyBoard() {
@@ -71,6 +60,7 @@ solve(): boolean {
         this.board[nextEmptyCell.row][nextEmptyCell.col].value = i;
 
         if (this.solve()) {
+
           return true;
         }
         //await delay(300);
--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { Cell } from './cell';
 
 @Component({
@@ -12,9 +11,10 @@ export class CellComponent implements OnInit {
   @Input() cell : Cell;
   @Input() isLocked: boolean;
 
-  // cellValue: FormControl;
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit(): void {
     // this.cellValue = new FormControl('');
